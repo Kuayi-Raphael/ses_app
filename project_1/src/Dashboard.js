91cdb7bbedbf3d65e92bf72d91af6060f972de6d
Kuayi Raphael <tinacity111@gmail.com>
2023-06-11 21:18:07 +0100

    first commit

---
+++ project_1/src/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Dashboard.css'; // Import the CSS file for styling
+
+const Dashboard = () => {
+    return <div className="dashboard-container"></div>;
+};
+
+export default Dashboard;
